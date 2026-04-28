c1677a7b2ed340d85b237b23dff8e55636d4bb62
Beatryz Caroline dos Santos Rodrigues <beatryzsenai2312@gmail.com>
2026-04-28 08:53:56 -0300

    base+ API pura

---
+++ Back-End/app.js
@@ -0,0 +1,22 @@
+
+// Carregar variáveis de ambiente ANTES de tudo
+require('dotenv').config();
+
+const express = require('express');
+const app = express(); 
+const produtosRoutes = require('./src/routes/produtoRoutes');  
+ 
+// Middleware para servir os arquivos estáticos do front-end 
+app.use(express.static('./public')); 
+ 
+// Middleware para interpretar JSON no corpo das requisições 
+app.use(express.json()); 
+ 
+// Aplica as rotas de cliente com o prefixo '/clientes' 
+// O caminho '/' no clientesRoutes.js se torna '/clientes' aqui. 
+app.use('/produtos', produtosRoutes);  
+ 
+// Inicia o servidor na porta 3000 
+app.listen(3000, () => { 
+    console.log('Servidor rodando em http://localhost:3000'); 
+});
---
+++ README.md
@@ -0,0 +1,11 @@
+Projeto final de integração de API produtos e programação front-end via react
+
+=======================
+Grupo 3- Integrantes
+=======================
+
+Beatryz Caroline dos Santos Rodrigues
+Beatriz Santos de Oliveira
+Pietra Del Vecchio Pivatti
+Isadora Gregatti Danelon 
+Marcela Pereira da Cruz
