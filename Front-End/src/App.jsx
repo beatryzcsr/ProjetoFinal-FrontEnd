0bfb29e417f43b3236d17d9b9f8e549619f662f1
Isadora-Danelon <isadora.danelon@aluno.senai.br>
2026-04-28 12:10:47 -0300

    COMMIT REACT-ROUTER-DOM

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,7 +1,6 @@
 import { Formulario, Home } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home/home'
-import Formulario from './pages/Formulario/formulario'
+
 
 
 function App() {
