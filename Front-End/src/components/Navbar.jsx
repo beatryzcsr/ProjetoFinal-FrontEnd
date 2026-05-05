5be7f5fe29539dfaec7740ed7eee932f0cde1fd0
Beatryz Caroline dos Santos Rodrigues <beatryzsenai2312@gmail.com>
2026-05-05 07:30:32 -0300

    novo codigo

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -3,11 +3,10 @@ import { NavLink } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <span className="navbar-brand">🔧 Sistema de Produtos</span>
       <div className="navbar-links">
         <NavLink to="/"         className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Início</NavLink>
+        <img src="./public/logo.png" alt="Logo" className = "logo"  />
         <NavLink to="/produtos" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Produtos</NavLink>
-        <NavLink to="/cadastro" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Cadastrar</NavLink>
       </div>
     </nav>
   );
